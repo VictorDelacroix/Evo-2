b6c97e1b316ec1e70e43ddd0fb478775aff6b9c1
Vincenzo <vincenzogreggio@hotmail.com>
2016-05-05 14:51:29 -0500

    Probando añadir Asp Mvc References

---
+++ Proyecto/UPCNETSystemCliente 2/UPCNETSystemCliente/UPCNETSystemCliente/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UPCNETSystemCliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
