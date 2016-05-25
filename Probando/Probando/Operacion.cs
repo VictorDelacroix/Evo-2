a2b3e85ed42b69a22d5d88bcf0a0fd75407a832f
Vincenzo Greggio <vincenzogreggio@hotmail.com>
2016-05-25 06:48:52 -0500

    Probaaando

---
+++ Probando/Probando/Operacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probando
+{
+    public class Operacion
+    {
+        private int a, b;
+
+        public Operacion ()
+        {
+           
+        }
+
+        public int Suma() { return a + b; }
+        public int Resta1() { return a - b; }
+        public int Resta2() { return b - a; }
+        public void setA(int a1) { a = a1; }
+        public void setB(int b1) { b = b1; }
+        public bool Iguales(int a, int b) { if (a == b) return true;  return false; }
+        public String Iguales(String a, String b) { if (a == b) return "Iguales";  return "Diferentes"; }
+
+    }
+
+
+}
---
+++ Probando/Probando/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probando
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Operacion ObjOperacion = new Operacion();
+
+        }
+
+        public bool Prueba1_Iguales_Suma(int a, int b, int REsperado)
+        {
+            Operacion ObjOperacion = new Operacion();
+            ObjOperacion.setA(a); ObjOperacion.setB(b);
+            if (ObjOperacion.Suma() == REsperado) return true; return false;
+        }
+    }
+}
